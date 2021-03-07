ac2e9747f1a2e53c88d837fbb01c387474405639
RodrigoMachado1106 <rodrigomachado1106@gmail.com>
2021-03-07 13:21:11 -0300

    Este é o segundo Exercicio de ED

---
+++ Ex02.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int idade;
+	
+	cout << "Entre com a idade do nadado: ";
+	cin  >> idade;
+	
+	while(idade < 5) {
+		cout << "Entre com a idade do nadado: ";
+	cin  >> idade;
+	}
+	
+	if(idade >= 5 && idade <= 7) {
+		cout << "Categoria Infantil A!";
+	} else if(idade >= 8 && idade <= 10) {
+		cout << "Categoria Infantil B!";	
+	} else if(idade >= 11 && idade <= 13) {
+		   cout << "Categoria Juvenil A!";	
+	} else if(idade >= 14 && idade <= 17){
+			cout << "Categoria Juvenil B!";
+	} else {
+		cout << " Categoria Sênior!";
+	}
+
+}
