1ed4088396f5532c2ba3c8f13279242044545a43
RodrigoMachado1106 <rodrigomachado1106@gmail.com>
2021-03-10 21:18:45 -0300

    Este é o sexto exercício de ED

---
+++ Ex06.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+	int numeros = 0;
+	int quadrado;
+	
+for(int numeros = 1; numeros <= 20; numeros++){
+	quadrado = numeros * numeros;
+	cout << quadrado << " ";
+}
+	
+	
+	
+	
+}
